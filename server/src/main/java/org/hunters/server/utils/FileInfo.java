f1a80a7c2c71d8e883b098850cc40c9533a463bf
djordjetane <djordje.tanaskovic1998@gmail.com>
2022-01-08 17:59:40 +0100

    Fixed module names

--- server/src/main/java/org/hunters/server/utils/FileInfo.java
+++ server/src/main/java/org/hunters/server/utils/FileInfo.java
@@ -14,10 +14,10 @@ public final class FileInfo {
 
     public final static String PUBLIC_HTML_DIR = System.getenv("PUBLIC_HTML") != null ?
                                                                                 System.getenv("PUBLIC_HTML")
-                                                                             :  "Client/dist/client";
+                                                                             :  "client/dist/client";
     public final static String RESOURCES_DIR = System.getenv("RESOURCES") != null ?
                                                                                 System.getenv("RESOURCES")
-                                                                             :  "Server/src/main/resources";
+                                                                             :  "server/src/main/resources";
 
     public static FileInfo get(Path path, Charset encoding) throws IOException {
         try (var fin = new FileInputStream(path.toString())){
