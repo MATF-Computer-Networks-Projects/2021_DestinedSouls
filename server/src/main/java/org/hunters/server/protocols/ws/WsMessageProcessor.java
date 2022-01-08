aa698411e8b98c4eff0669f3c9f2515d4d9467ac
djordjetane <djordje.tanaskovic1998@gmail.com>
2022-01-09 00:46:59 +0100

    Replaced response and msg to user

--- server/src/main/java/org/hunters/server/protocols/ws/WsMessageProcessor.java
+++ server/src/main/java/org/hunters/server/protocols/ws/WsMessageProcessor.java
@@ -33,21 +33,17 @@ public class WsMessageProcessor {
             return;
         }
 
-        //response.writeToMessage(Responses.wsResponse(frame.encoded(), "{}"));
-        //writeProxy.enqueue(response);
-        var responseToSocket = writeProxy.getMessage();
-        responseToSocket.socketId = request.socketId;
-        responseToSocket.writeToMessage(Responses.wsResponse(frame.encoded(), "{\"id\":\"" + res.json.get("id") + "\"}"));
-        writeProxy.enqueue(responseToSocket);
+        response.writeToMessage( Responses.wsResponse(frame.encoded(), "{\"id\":\"" + res.json.get("id") + "\"}") );
+        writeProxy.enqueue(response);
 
         int targetSocketId = Integer.parseInt(res.json.remove("socketId"));
-        response.socketId = targetSocketId;
         if(targetSocketId != -1) {
-            response.writeToMessage( Responses.wsResponse(frame.encoded(),  res.json.toString() ) );
-            writeProxy.enqueue(response);
+            var messageToUser = writeProxy.getMessage();
+            messageToUser.socketId = targetSocketId;
+            messageToUser.writeToMessage(Responses.wsResponse(frame.encoded(),  res.json.toString() ));
+            writeProxy.enqueue(messageToUser);
         }
     }
-
 
 
     private String appendSocketId(Message request, int endIdx) {
